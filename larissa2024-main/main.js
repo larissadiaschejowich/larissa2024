a8feafed8cf80eef09bb0eb361d7d2cefc35cd9f
larissadiaschejowich <129761676+larissadiaschejowich@users.noreply.github.com>
2024-03-22 16:42:52 -0300

    Add files via upload

--- larissa2024-main/main.js
+++ larissa2024-main/main.js
@@ -1,13 +1,13 @@
 const botoes=document.querySelectoraAll(".botao");
 console.log(botoes)
-for (let i=0; i < botões.length; i++) {
-}
-    botoes[i].onclick=function(){
 
-for(let j=0;j<botoes.length;j++){
-    botoes[j].claList.remove("ativo");
+for (let i=0; i < botoes.length; i++) {
+    console.log(i)
 }
-botoes[i].classList.add("ativo");
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function() {
+       botoes[i].classList.add(ativo);
     }
+}
         
 
