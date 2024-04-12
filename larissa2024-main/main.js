927137ff0b8b5f73c92dae5f5072838f48ae6da6
larissadiaschejowich <129761676+larissadiaschejowich@users.noreply.github.com>
2024-04-12 16:39:12 -0300

    Add files via upload

--- larissa2024-main/main.js
+++ larissa2024-main/main.js
@@ -1,4 +1,5 @@
 const botoes=document.querySelectoraAll(".botao");
+const textos=document.querySelectorAll(".aba-conteudo");
 console.log(botoes)
 
 for (let i=0; i < botoes.length; i++) {
@@ -6,11 +7,13 @@ for (let i=0; i < botoes.length; i++) {
          
         for(let j=0;j<botoes.length;j++) { 
             botoes[j].classList.remove(ativo);
+            textos[j].classList.remove(ativo)
     }
     console.log(i)
 }
         botoes[i].classList.add(ativo);
-    }
-
-        
+        textos[i].classList.add(ativo);
+}
+const contadores = document.querySelectoraAll(".contador");
+contadores[0].textContent="contagem regressiva";
 
